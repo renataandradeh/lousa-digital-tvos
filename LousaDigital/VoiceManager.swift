ac01ea69549951dd070aad7e17e0283fcf8fead3
Matheus Vasconcelos de Sousa <matheusvds@Matheuss-MacBook-Pro.local>
2017-02-20 10:45:04 -0300

    added voice

---
+++ LousaDigital/VoiceManager.swift
@@ -0,0 +1,40 @@
+//
+//  VoiceManager.swift
+//  LousaDigital
+//
+//  Created by Matheus Vasconcelos de Sousa on 20/02/17.
+//  Copyright © 2017 Renata Gondim Andrade Theóphilo. All rights reserved.
+//
+
+import AVFoundation
+
+/* HOW TO USE
+ 
+ Instance class:
+ let owl = VoiceManager()
+ 
+ Speak:
+ owl.speak("hello")
+ 
+ */
+
+class VoiceManager {
+    
+    private let speechSynthesizer : AVSpeechSynthesizer
+    private let speechPitch : Float
+    private let speechLanguage : AVSpeechSynthesisVoice
+    
+    init() {
+        speechSynthesizer = AVSpeechSynthesizer()
+        speechPitch = 1.5
+        speechLanguage = AVSpeechSynthesisVoice(identifier: "en-US")!
+    }
+    
+    private func speak(_ text: String) {
+        let speechExpression = AVSpeechUtterance(string: text)
+        speechExpression.rate = self.speechPitch
+        speechExpression.voice = self.speechLanguage
+        speechSynthesizer.speak(speechExpression)
+    }
+    
+}
