2cc7e0b607b085e9f9a28356960f0e6c759bea1e
Renata Gondim Andrade Theóphilo <renataandradeh@gmail.com>
2017-02-25 23:00:33 -0300

    Concluindo a primeira parte da animação da atividade de conhecer as vogais.

--- LousaDigital/LettersGame.swift
+++ LousaDigital/LettersGame.swift
@@ -12,10 +12,12 @@ class LettersGame: ActivityScene {
     
     var object: SKSpriteNode?
     var letterBox: SKSpriteNode?
-    var letters : [Button] = []
+    
+    var letters : [SKSpriteNode] = []
   
     var selectedLetter : String!
-    //let fullNameArr : [String]!
+    let firstMomentDuration : Double = 8
+    let secondMomentDuration : Double = 8
     
     override func didMove(to: SKView) {
         
@@ -28,34 +30,56 @@ class LettersGame: ActivityScene {
         //Criando e adicionando os gestures à view
         self.createGestures(view: self.view!, actionTouch: #selector(pressedSelect), actionPlay: #selector(pressedPlay))
         
-        for i in 1...5{
-            letters.append(childNode(withName: "letter\(i)") as! Button)
-        }
+        //Preenchendo um array com todas as letras da cena
+        letters = self.scene?["letter1_*"] as! [SKSpriteNode]
+        
 
+        //Action customizada que checa letra por letra se sua animação terminou para poder falar
+        var lettersCopy = letters
+        
+        let firstMomentActions = SKAction.customAction(withDuration: firstMomentDuration, actionBlock: { (_, _) in
+            if let nextLetter = lettersCopy.first, !nextLetter.hasActions(), let letterName = nextLetter.name {
+                owl.speak(ButtonType(buttonName: letterName.replacingOccurrences(of: "1_", with: "")).rawValue)
+                lettersCopy.removeFirst()
+            }
+        })
+        
+        let fadeOut = SKAction.run({
+            for letter in self.letters {
+                letter.run(SKAction.fadeAlpha(to: 0, duration: 1.0))
+            }
+        })
+        
+        let secondMomentActions = SKAction.customAction(withDuration: secondMomentDuration, actionBlock: { (_, _) in
+            
+        })
+        
+        run(SKAction.sequence([firstMomentActions, fadeOut]))
+    }
+    
+    func processActionForMoment(moment: Int, array: [SKSpriteNode]) {
+        
     }
     
     func pressedSelect(){
         
-        for letter in letters{
-            if letter.isFocused  {
-                
-                selectedLetter = letter.name
-                
-                let fullNameArr = selectedLetter.characters.split{$0 == "r"}.map(String.init)
-                 object?.texture = SKTexture(imageNamed: "object\(fullNameArr[1])")
-                letterBox?.texture = SKTexture(imageNamed: selectedLetter)
-                //animação da letra movendo
-                //self.deactivateLettersFocuses()
-  
-            }
-        }
+//        for letter in letters{
+//            if letter.isFocused  {
+//                
+//                selectedLetter = letter.name
+//                
+//                let fullNameArr = selectedLetter.characters.split{$0 == "r"}.map(String.init)
+//                 object?.texture = SKTexture(imageNamed: "object\(fullNameArr[1])")
+//                letterBox?.texture = SKTexture(imageNamed: selectedLetter)
+//            }
+//        }
     }
     
     func pressedPlay(){
-        for letter in letters{
-            if letter.isFocused{
-                owl.speak((letter.buttonType?.rawValue)!)
-            }
-        }
+//        for letter in letters{
+//            if letter.isFocused{
+//                owl.speak((letter.buttonType?.rawValue)!)
+//            }
+//        }
     }
 }
